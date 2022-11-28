3e8b4cbf316c798b1ab9038fa48d4f8b1e99ddcb
Traven Reese <82552048+Travenedu@users.noreply.github.com>
2022-11-28 18:14:57 -0500

    Update signal.c

--- signal.c
+++ signal.c
@@ -3,21 +3,26 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdbool.h>
+//Collaborated with Peirce Medy & Izu
+
+bool trigger = false; 
 
 void handler(int signum)
 { //signal handler
   printf("Hello World!\n");
-  //exit(1); //exit after printing
+  trigger = true;
+  alarm(1);
 }
 
 int main(int argc, char * argv[])
 {
+  for(;;){
+    trigger = false;
   signal(SIGALRM,handler); //register handler to handle SIGALRM
-  do{
-  alarm(1); //Schedule a SIGALRM for 1 second
-  sleep(1);
-    printf("Turing was right!\n");
-  } while(1); //busy wait for signal to be delivered
-  return 0; //never reached
+  alarm(1);
+  while(trigger == false);
+  printf("Turing was right!\n");
+  }
+  return 0;
 }
-
