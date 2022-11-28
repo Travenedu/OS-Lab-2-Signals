22110a46801bd6d60934e5745de77e04a72e00b1
Traven Reese <82552048+Travenedu@users.noreply.github.com>
2022-11-28 18:12:40 -0500

    Update timer.c

--- timer.c
+++ timer.c
@@ -1,34 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <signal.h>
+#include <stdbool.h>
+//Collaborated with Pierce Medy & Izu 
 
-time_t start_time;
-time_t end_time;
-time_t total;
-int number_alarm = 0;
+bool trigger = false;
+int counter = 0;
+time_t start;
+time_t end;
+
 
 void handler(int signum){
   printf("Hello World!\n");
+  trigger = true;
+  alarm(1);
+  counter += 1;
 }
 
-
-void handler(int sec){
-  end_time = time(NULL);
-  total = end_time - start_time;
-  printf("\nNumber of alarms : &d \n, number_alarm");
-  printf("The program was excuting for: %ld seconds \n", total);
-  exit(1)
+void time_handler(int second){
+  end = time(NULL);
+  int diff = (end - start);
+  printf("Total time in seconds: %d\n", diff);
+  printf("Total time in executed: %d\n", counter);
+  exit(1);
 }
 
 int main(int argc, char * argv[]){
-  start_time = time(NULL;)
-  signal(SIGALRM,handler);
-  signal(SIGINT,handler_sigint);
-  do{
+  start = time(NULL);
+  signal(SIGALRM, time_handler); 
+  for(;;){
+    trigger = false;
+    signal(SIGALRM, handler);
     alarm(1);
-    num_alarms +=1;
-    sleep(1);
-    print("Turning was right!\n");
-  } while(1);
-
+    while(trigger == false);
+    printf("Turing was right!\n");
+  }
   return 0;
 }
